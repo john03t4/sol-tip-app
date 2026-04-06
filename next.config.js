4670e865ebea1f4a9f4dded21ec05a5da3a368ac
john03t4 <john03t4@gmail.com>
2026-04-06 19:05:21 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Vercel handles env vars automatically
+  env: {},
+};
+
+module.exports = nextConfig;
